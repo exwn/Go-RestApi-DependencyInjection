ef31f22969c34b92a30d756877b0dc1ef687b03c
exwn <ixwanull.4za@outlook.com>
2022-10-11 10:52:11 +0700

    feat: setting up database & migrate

---
+++ database/db.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"Assignment_02/models"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "8525"
+	dbPort   = "5432"
+	dbName   = "golangdb"
+	db       *gorm.DB
+	err      error
+)
+
+func StartDB() {
+	config := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=disable", host, user, password, dbPort, dbName)
+
+	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database : ", err)
+	}
+	db.Debug().AutoMigrate(models.Order{}, models.Item{})
+}
+func GetDB() *gorm.DB {
+	return db
+}
