a29e7109366811cc6f261642e9a48384c691b43d
exwn <ixwanull.4za@outlook.com>
2022-10-12 13:25:44 +0700

    feat: implementation dependency injection

---
+++ controllers/init.go
@@ -0,0 +1,13 @@
+package controllers
+
+import "gorm.io/gorm"
+
+type Controllers struct {
+	masterDB *gorm.DB
+}
+
+func New(db *gorm.DB) *Controllers {
+	return &Controllers{
+		masterDB: db,
+	}
+}
